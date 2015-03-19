a9e5cd6bda4c015cfa9287c1a30b1bc722b88cfa
stevenmiller888 <stevenmiller888@me.com>
2015-03-18 20:50:09 -0700

    updated build/foreground.js

--- build/foreground.js
+++ build/foreground.js
@@ -109,9 +109,9 @@ detective.boot();
 * Module dependencies.
 */
 
-var fmt     = require('yields/fmt');
-var each    = require('component/each');
 var cssPath = require('stevenmiller888/cssPath');
+var each = require('component/each');
+var fmt  = require('yields/fmt');
 
 /**
  * Expose `Detective`.
@@ -188,49 +188,98 @@ Detective.prototype.handle = function (event, node) {
   chrome.runtime.sendMessage(message);
 };
 
-}, {"yields/fmt":3,"component/each":4,"stevenmiller888/cssPath":5}],
+}, {"stevenmiller888/cssPath":3,"component/each":4,"yields/fmt":5}],
 3: [function(require, module, exports) {
 
 /**
- * toString.
+ * Expose `cssPath`.
  */
 
-var toString = window.JSON
-  ? JSON.stringify
-  : function(_){ return String(_); };
+module.exports = cssPath;
 
 /**
- * Export `fmt`
- */
-
-module.exports = fmt;
-
-/**
- * Formatters
- */
-
-fmt.o = toString;
-fmt.s = String;
-fmt.d = parseInt;
-
-/**
- * Format the given `str`.
+ * Get full CSS path of any element.
  *
- * @param {String} str
- * @param {...} args
+ * @param {Node} el
  * @return {String}
- * @api public
  */
 
-function fmt(str){
-  var args = [].slice.call(arguments, 1);
-  var j = 0;
+function cssPath(el) {
+  var fullPath = 0, // Set to 1 to build ultra-specific full CSS-path, or 0 for optimised selector
+    useNthChild = 0, // Set to 1 to use ":nth-child()" pseudo-selectors to match the given element
+    cssPathStr = '',
+    testPath = '',
+    parents = [],
+    parentSelectors = [],
+    tagName,
+    cssId,
+    cssClass,
+    tagSelector,
+    vagueMatch,
+    nth,
+    i,
+    c;
 
-  return str.replace(/%([a-z])/gi, function(_, f){
-    return fmt[f]
-      ? fmt[f](args[j++])
-      : _ + f;
-  });
+  // Go up the list of parent nodes and build unique identifier for each:
+  while (el) {
+    vagueMatch = 0;
+
+    // Get the node's HTML tag name in lowercase:
+    tagName = el.nodeName.toLowerCase();
+
+    // Get node's ID attribute, adding a '#':
+    cssId = (el.id) ? ('#' + el.id) : false;
+
+    // Get node's CSS classes, replacing spaces with '.':
+    cssClass = (el.className) ? ('.' + el.className.replace(/\s+/g, ".")) : '';
+
+    // Build a unique identifier for this parent node:
+    if (cssId) {
+      // Matched by ID:
+      tagSelector = tagName + cssId + cssClass;
+    } else if (cssClass) {
+      // Matched by class (will be checked for multiples afterwards):
+      tagSelector = tagName + cssClass;
+    } else {
+      // Couldn't match by ID or class, so use ":nth-child()" instead:
+      vagueMatch = 1;
+      tagSelector = tagName;
+    }
+
+    // Add this full tag selector to the parentSelectors array:
+    parentSelectors.unshift(tagSelector)
+
+    // If doing short/optimised CSS paths and this element has an ID, stop here:
+    if (cssId && !fullPath)
+      break;
+
+    // Go up to the next parent node:
+    el = el.parentNode !== document ? el.parentNode : false;
+
+  } // endwhile
+
+  // Build the CSS path string from the parent tag selectors:
+  for (i = 0; i < parentSelectors.length; i++) {
+    cssPathStr += ' ' + parentSelectors[i]; // + ' ' + cssPathStr;
+
+    // If using ":nth-child()" selectors and this selector has no ID / isn't the html or body tag:
+    if (useNthChild && !parentSelectors[i].match(/#/) && !parentSelectors[i].match(/^(html|body)$/)) {
+
+      // If there's no CSS class, or if the semi-complete CSS selector path matches multiple elements:
+      if (!parentSelectors[i].match(/\./) || $(cssPathStr).length > 1) {
+
+        // Count element's previous siblings for ":nth-child" pseudo-selector:
+        for (nth = 1, c = el; c.previousElementSibling; c = c.previousElementSibling, nth++);
+
+        // Append ":nth-child()" to CSS path:
+        cssPathStr += ":nth-child(" + nth + ")";
+      }
+    }
+
+  }
+
+  // Return trimmed full CSS path:
+  return cssPathStr.replace(/^[ \t]+|[ \t]+$/, '');
 }
 
 }, {}],
@@ -607,94 +656,45 @@ function prefixed(str) {
 5: [function(require, module, exports) {
 
 /**
- * Expose `cssPath`.
+ * toString.
  */
 
-module.exports = cssPath;
+var toString = window.JSON
+  ? JSON.stringify
+  : function(_){ return String(_); };
 
 /**
- * Get full CSS path of any element.
- *
- * @param {Node} el
- * @return {String}
+ * Export `fmt`
  */
 
-function cssPath(el) {
-  var fullPath = 0, // Set to 1 to build ultra-specific full CSS-path, or 0 for optimised selector
-    useNthChild = 0, // Set to 1 to use ":nth-child()" pseudo-selectors to match the given element
-    cssPathStr = '',
-    testPath = '',
-    parents = [],
-    parentSelectors = [],
-    tagName,
-    cssId,
-    cssClass,
-    tagSelector,
-    vagueMatch,
-    nth,
-    i,
-    c;
+module.exports = fmt;
 
-  // Go up the list of parent nodes and build unique identifier for each:
-  while (el) {
-    vagueMatch = 0;
+/**
+ * Formatters
+ */
 
-    // Get the node's HTML tag name in lowercase:
-    tagName = el.nodeName.toLowerCase();
+fmt.o = toString;
+fmt.s = String;
+fmt.d = parseInt;
 
-    // Get node's ID attribute, adding a '#':
-    cssId = (el.id) ? ('#' + el.id) : false;
+/**
+ * Format the given `str`.
+ *
+ * @param {String} str
+ * @param {...} args
+ * @return {String}
+ * @api public
+ */
 
-    // Get node's CSS classes, replacing spaces with '.':
-    cssClass = (el.className) ? ('.' + el.className.replace(/\s+/g, ".")) : '';
+function fmt(str){
+  var args = [].slice.call(arguments, 1);
+  var j = 0;
 
-    // Build a unique identifier for this parent node:
-    if (cssId) {
-      // Matched by ID:
-      tagSelector = tagName + cssId + cssClass;
-    } else if (cssClass) {
-      // Matched by class (will be checked for multiples afterwards):
-      tagSelector = tagName + cssClass;
-    } else {
-      // Couldn't match by ID or class, so use ":nth-child()" instead:
-      vagueMatch = 1;
-      tagSelector = tagName;
-    }
-
-    // Add this full tag selector to the parentSelectors array:
-    parentSelectors.unshift(tagSelector)
-
-    // If doing short/optimised CSS paths and this element has an ID, stop here:
-    if (cssId && !fullPath)
-      break;
-
-    // Go up to the next parent node:
-    el = el.parentNode !== document ? el.parentNode : false;
-
-  } // endwhile
-
-  // Build the CSS path string from the parent tag selectors:
-  for (i = 0; i < parentSelectors.length; i++) {
-    cssPathStr += ' ' + parentSelectors[i]; // + ' ' + cssPathStr;
-
-    // If using ":nth-child()" selectors and this selector has no ID / isn't the html or body tag:
-    if (useNthChild && !parentSelectors[i].match(/#/) && !parentSelectors[i].match(/^(html|body)$/)) {
-
-      // If there's no CSS class, or if the semi-complete CSS selector path matches multiple elements:
-      if (!parentSelectors[i].match(/\./) || $(cssPathStr).length > 1) {
-
-        // Count element's previous siblings for ":nth-child" pseudo-selector:
-        for (nth = 1, c = el; c.previousElementSibling; c = c.previousElementSibling, nth++);
-
-        // Append ":nth-child()" to CSS path:
-        cssPathStr += ":nth-child(" + nth + ")";
-      }
-    }
-
-  }
-
-  // Return trimmed full CSS path:
-  return cssPathStr.replace(/^[ \t]+|[ \t]+$/, '');
+  return str.replace(/%([a-z])/gi, function(_, f){
+    return fmt[f]
+      ? fmt[f](args[j++])
+      : _ + f;
+  });
 }
 
 }, {}]}, {}, {"1":""})
