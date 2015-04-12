b0520dc1a8b640d6b22da2fad8861192d46d4210
stevenmiller888 <stevenmiller888@me.com>
2015-04-12 00:48:50 -0700

    updated app/background/recorder.js

--- app/background/recorder.js
+++ app/background/recorder.js
@@ -13,13 +13,9 @@ var each = require('component/each');
  */
 
 var analytics = new Analytics('J0KCCfAPH6oXQJ8Np1IwI0HgAGW5oFOX');
-
-var userId = localStorage['userId'];
-
-if (!userId) {
-  userId = uid();
-  localStorage['userId'] = userId;
-}
+var oldId = localStorage['userId'];
+var newId = oldId || uid();
+if (!oldId) localStorage['userId'] = newId;
 
 /**
  * Expose `Recorder`.
@@ -31,10 +27,9 @@ module.exports = Recorder;
  * Recorder.
  */
 
-function Recorder () {
+function Recorder(){
   if (!(this instanceof Recorder)) return new Recorder();
   this.recording = [];
-  return this;
 }
 
 /**
@@ -43,7 +38,7 @@ function Recorder () {
  * @param {String} message
  */
 
-Recorder.prototype.record = function (message) {
+Recorder.prototype.record = function(message){
   var lastElement = this.recording[this.recording.length - 1];
   if (!lastElement) return this.recording.push(message);
   if (lastElement[1] === message[1]) return;
@@ -54,14 +49,13 @@ Recorder.prototype.record = function (message) {
  * Start recording.
  */
 
-Recorder.prototype.startRecording = function () {
-  analytics.track({
-    userId: userId,
-    event: 'Started recording',
-    background: true
-  });
+Recorder.prototype.startRecording = function(){
   var self = this;
-  self.detect();
+
+  this.detectScreenshots();
+  this.detectUrl();
+  this.detectEvents();
+
   chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     var message = request;
     self.record(message);
@@ -69,24 +63,15 @@ Recorder.prototype.startRecording = function () {
 };
 
 /**
- * Detect.
- */
-
-Recorder.prototype.detect = function () {
-  this.detectScreenshots();
-  this.detectUrl();
-  this.detectEvents();
-};
-
-/**
  * Record events on the page.
  */
 
-Recorder.prototype.detectEvents = function () {
-  chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+Recorder.prototype.detectEvents = function(){
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
     inject('foreground.js', tabs[0].id);
   });
-  chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+
+  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
     if (changeInfo.status == 'complete') {
       chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
         if (tabId === tabs[0].id) inject('foreground.js', tabs[0].id);
@@ -96,44 +81,27 @@ Recorder.prototype.detectEvents = function () {
 };
 
 /**
- * Detect the Url.
- *
+ * Detect the url.
  */
 
-Recorder.prototype.detectUrl = function () {
+Recorder.prototype.detectUrl = function(){
   var self = this;
-  chrome.webNavigation.onCommitted.addListener(function (details) {
+  
+  chrome.webNavigation.onCommitted.addListener(function(details){
     var type = details.transitionType;
     var from = details.transitionQualifiers;
+    
     switch (type) {
       case 'reload':
-        analytics.track({
-          userId: userId,
-          event: 'Changed Url',
-          type: 'reload',
-          background: true
-        });
         if (!self.recording.length) return self.record(["goto", details.url]);
         self.record(['reload']);
         break;
       case 'typed':
-        analytics.track({
-          userId: userId,
-          event: 'Changed Url',
-          type: 'type',
-          background: true
-        });
         if (!from.length) return self.record(["goto", details.url]);
         if (from[0] === "from_address_bar") return self.record(["goto", details.url]);
         if (from[0] === "server_redirect" && from[1] === "from_address_bar") return self.record(["goto", details.url]);
         break;
       case 'auto_bookmark':
-        analytics.track({
-          userId: userId,
-          event: 'Changed Url',
-          type: 'bookmark',
-          background: true
-        });
         self.record(["goto", details.url]);
         break;
     }
@@ -144,17 +112,11 @@ Recorder.prototype.detectUrl = function () {
  * Detect screenshots.
  */
 
-Recorder.prototype.detectScreenshots = function () {
+Recorder.prototype.detectScreenshots = function(){
   var self = this;
-  chrome.commands.onCommand.addListener(function (command) {
-    if (command === "detect-screenshot") {
-      analytics.track({
-        userId: userId,
-        event: 'Took Screenshot',
-        background: true
-      });
-      self.record(['screenshot', 'index.png']);
-    }
+  
+  chrome.commands.onCommand.addListener(function(command){
+    if (command === "detect-screenshot") self.record(['screenshot', 'index.png']);
   });
 };
 
@@ -162,7 +124,7 @@ Recorder.prototype.detectScreenshots = function () {
  * Stop recording.
  */
 
-Recorder.prototype.stopRecording = function () {
+Recorder.prototype.stopRecording = function(){
   chrome.commands.onCommand.removeListener();
   chrome.webNavigation.onCommitted.removeListener();
   chrome.runtime.onMessage.removeListener();
@@ -176,6 +138,6 @@ Recorder.prototype.stopRecording = function () {
  * @param {Number} id
  */
 
-function inject (name, id) {
+function inject(name, id){
   chrome.tabs.executeScript(id, {file: name});
 };
