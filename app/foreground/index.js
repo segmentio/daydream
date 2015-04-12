e77cdcdae84266cbe8bc4a5673f7ce187d3af708
stevenmiller888 <stevenmiller888@me.com>
2015-04-12 00:48:50 -0700

    updated app/foreground/index.js

--- app/foreground/index.js
+++ app/foreground/index.js
@@ -1,18 +1,64 @@
 
 /**
- * Module dependencies.
- */
+* Module dependencies.
+*/
 
-var Detective = require('./detective');
-
-/**
- * Initialize a new Detective.
- */
-
-var detective = Detective();
+var cssPath = require('stevenmiller888/cssPath');
+var each = require('component/each');
+var fmt  = require('yields/fmt');
 
 /**
- * Start detecting.
+ * Events and elements.
  */
 
-detective.boot();
+detect(['a', 'button'], 'click');
+detect(['input'], 'keydown');
+detect([], 'copy');
+
+/**
+ * Detect.
+ *
+ * @param {Array} tagArr
+ * @param {String} listener
+ */
+
+function detect(tagArr, listener){
+  if (!tagArr.length) return copyText();
+
+  each(tagArr, function(tag){
+    var els = document.getElementsByTagName(tag);
+    each(els, function(el){
+      el.addEventListener(listener, function(event){
+        if (listener === 'click') handle('click', event.target);
+        if (listener === 'keydown' && event.keyCode === 9) handle('type', event.target);
+      });
+    });
+  });
+};
+
+/**
+* Copy text.
+*/
+
+function copyText(){
+  window.onkeydown = function(event){
+    if (event.keyCode === 67 && event.ctrlKey) {
+      var selObj = window.getSelection();
+      handle('evaluate', selObj.focusNode);
+    }
+  };
+};
+
+/**
+ * Handle.
+ *
+ * @param {String} event
+ * @param {Node} node
+ */
+
+function handle(event, node){
+  var path = cssPath(node);
+  var message = [event, path];
+  if (node.value) message.push(node.value);
+  chrome.runtime.sendMessage(message);
+};
