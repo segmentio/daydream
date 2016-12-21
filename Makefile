b062867a928d602bf54339ffcc29dfe62652c961
Steven Miller <stevenmiller888@me.com>
2016-12-21 01:53:00 -0800

    converted to webpack

--- Makefile
+++ Makefile
@@ -1,31 +1,16 @@
-
-BIN = node_modules/.bin
-DUO = $(BIN)/duo
-
-BG := $(shell find lib/background/index.js)
-FG := $(shell find lib/foreground/index.js)
-
-default: build
+WEBPACK_FLAGS ?=
 
 build: node_modules
-	@mkdir -p build
-	@mkdir -p build/images
-	@cp lib/manifest.json build/manifest.json
-	@cp lib/images/icon-black.png build/images/icon-black.png
-	@cp lib/images/icon-green.png build/images/icon-green.png
-	@cp lib/foreground/popup/index.html build/index.html
-	@cp lib/foreground/popup/index.css build/index.css
-	@$(DUO) lib/foreground/popup/index.js > build/index.js
-	@$(DUO) $(BG) > build/background.js
-	@$(DUO) $(FG) > build/foreground.js
+	node_modules/.bin/webpack $(WEBPACK_FLAGS) --bail
 
 node_modules: package.json
-	@npm install
+	npm install
+	touch $@
 
 clean:
-	@rm -rf build
+	rm -rf build
 
-distclean:
-	@rm -rf components node_modules
+lint: node_modules
+	node_modules/.bin/standard
 
-.PHONY: clean
+.PHONY: clean lint
---
+++ webpack.config.babel.js
@@ -0,0 +1,46 @@
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import CopyPlugin from 'copy-webpack-plugin'
+import cssImports from 'postcss-import'
+import cssnext from 'postcss-cssnext'
+import path from 'path'
+
+const base = {
+  context: path.join(__dirname, 'lib'),
+  entry: {
+    background: './background/index.js',
+    'content-script': './content-scripts/index.js',
+    popup: './popup/index.js'
+  },
+  output: {
+    path: './build',
+    publicPath: '/',
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss', { publicPath: '/' })
+      }
+    ]
+  },
+  postcss: webpack => [
+    cssImports({ addDependencyTo: webpack }),
+    cssnext({ browsers: 'last 2 versions' })
+  ],
+  plugins: [
+    new CopyPlugin([
+      { from: './popup/index.html', to: './popup.html' },
+      { from: './manifest.json', to: './manifest.json' },
+      { from: 'images', to: 'images' }
+    ]),
+    new ExtractTextPlugin('[name].css')
+  ]
+}
+
+module.exports = base
