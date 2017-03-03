794b95b0cb15cf16ce63b81dff62d1462e701ffe
Steven Miller <stevenmiller888@me.com>
2017-03-03 11:05:12 -0800

    add syntax highlighting back

--- src/popup/components/App.js
+++ src/popup/components/App.js
@@ -1,46 +1,45 @@
-
 import React from 'react'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import syntaxStyle from './syntaxStyle'
 import styles from './App.css'
 
 const App = props => (
   <div>
-    <pre className={styles.pre}>
-      <code>
-        {`const Nightmare = require('nightmare')
+    <SyntaxHighlighter language='javascript' style={syntaxStyle}>
+      {`const Nightmare = require('nightmare')
 const nightmare = Nightmare({ show: true })
 
 nightmare
-${props.recording.reduce((records, record, i) => {
-  const { action, url, selector, value } = record
-  let result = records
-  if (i !== records.length) result += '\n'
+  ${props.recording.reduce((records, record, i) => {
+    const { action, url, selector, value } = record
+    let result = records
+    if (i !== records.length) result += '\n'
 
-  switch (action) {
-    case 'change':
-      result += `.type('${selector}', '${value}')`
-      break
-    case 'click':
-      result += `.click('${selector}')`
-      break
-    case 'goto':
-      result += `.goto('${url}')`
-      break
-    case 'reload':
-      result += `.refresh()`
-      break
-  }
+    switch (action) {
+      case 'change':
+        result += `.type('${selector}', '${value}')`
+        break
+      case 'click':
+        result += `.click('${selector}')`
+        break
+      case 'goto':
+        result += `.goto('${url}')`
+        break
+      case 'reload':
+        result += `.refresh()`
+        break
+    }
 
-  return result
-}, '')}
-.end()
-.then(function (result) {
-  console.log(result)
-})
-.catch(function (error) {
-  console.error('Error:', error);
-});`}
-      </code>
-    </pre>
+    return result
+  }, '')}
+  .end()
+  .then(function (result) {
+    console.log(result)
+  })
+  .catch(function (error) {
+    console.error('Error:', error);
+  });`}
+    </SyntaxHighlighter>
 
     <button className={styles.button} onClick={props.handleRestart}>Restart</button>
   </div>
---
+++ src/popup/components/syntaxStyle.js
@@ -0,0 +1,20 @@
+
+export default {
+  hljs: {
+    fontSize: '13px',
+    overflow: 'auto',
+    margin: 0
+  },
+  'hljs-keyword': {
+    color: '#de6339'
+  },
+  'hljs-string': {
+    color: '#e2a413'
+  },
+  'hljs-built_in': {
+    color: '#49B882'
+  },
+  'hljs-literal': {
+    color: '#6c71c4'
+  }
+}
