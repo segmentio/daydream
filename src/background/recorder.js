cae464256c72ec100bee28186e97efdc95290b05
Steven Miller <stevenmiller888@me.com>
2017-03-04 14:02:14 -0800

    simplify the recorder

--- src/background/recorder.js
+++ src/background/recorder.js
@@ -5,10 +5,9 @@ export default class Recorder {
   }
 
   start () {
-    chrome.webNavigation.onCommitted.addListener(this.handleNavigation.bind(this))
-    chrome.tabs.onUpdated.addListener(this.handleTabUpdate.bind(this))
-    chrome.runtime.onMessage.addListener(this.record.bind(this))
-    chrome.tabs.executeScript({ file: 'content-script.js' })
+    chrome.webNavigation.onCompleted.addListener(this.handleCompletedNavigation.bind(this))
+    chrome.webNavigation.onCommitted.addListener(this.handleCommittedNavigation.bind(this))
+    chrome.runtime.onMessage.addListener(this.handleMessage.bind(this))
   }
 
   stop () {
@@ -17,23 +16,19 @@ export default class Recorder {
     chrome.tabs.onUpdated.removeListener()
   }
 
-  handleNavigation (details) {
-    const { transitionType, url } = details
-    if (transitionType === 'auto_bookmark' || transitionType === 'typed') {
-      this.record({ action: 'goto', url })
-    } else if (transitionType === 'reload') {
-      if (!this.recording.length) return this.record({ action: 'goto', url })
-      this.record({ action: 'reload' })
-    }
-  }
-
-  handleTabUpdate (_, { status }) {
-    if (status === 'complete') {
+  handleCompletedNavigation ({ url, frameId }) {
+    if (frameId === 0) {
       chrome.tabs.executeScript({ file: 'content-script.js' })
     }
   }
 
-  record (message) {
+  handleCommittedNavigation ({ transitionQualifiers, url }) {
+    if (transitionQualifiers.includes('from_address_bar')) {
+      this.handleMessage({ action: 'goto', url })
+    }
+  }
+
+  handleMessage (message) {
     this.recording.push(message)
   }
 }
--- src/popup/components/App.js
+++ src/popup/components/App.js
@@ -10,35 +10,35 @@ const App = props => (
 const nightmare = Nightmare({ show: true })
 
 nightmare
-  ${props.recording.reduce((records, record, i) => {
-    const { action, url, selector, value } = record
-    let result = records
-    if (i !== records.length) result += '\n'
+${props.recording.reduce((records, record, i) => {
+  const { action, url, selector, value } = record
+  let result = records
+  if (i !== records.length) result += '\n'
 
-    switch (action) {
-      case 'change':
-        result += `.type('${selector}', '${value}')`
-        break
-      case 'click':
-        result += `.click('${selector}')`
-        break
-      case 'goto':
-        result += `.goto('${url}')`
-        break
-      case 'reload':
-        result += `.refresh()`
-        break
-    }
+  switch (action) {
+    case 'change':
+      result += `.type('${selector}', '${value}')`
+      break
+    case 'click':
+      result += `.click('${selector}')`
+      break
+    case 'goto':
+      result += `.goto('${url}')`
+      break
+    case 'reload':
+      result += `.refresh()`
+      break
+  }
 
-    return result
-  }, '')}
-  .end()
-  .then(function (result) {
-    console.log(result)
-  })
-  .catch(function (error) {
-    console.error('Error:', error);
-  });`}
+  return result
+}, '')}
+.end()
+.then(function (result) {
+  console.log(result)
+})
+.catch(function (error) {
+  console.error('Error:', error);
+});`}
     </SyntaxHighlighter>
 
     <button className={styles.button} onClick={props.handleRestart}>Restart</button>
